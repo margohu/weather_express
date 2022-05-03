e682cc9b45053325c338199d509677a7a6db1c60
Margo Hutt <margo.hutt@ir.ee>
2022-05-03 09:07:46 +0300

    Created project basic files

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 const path = require('path')
 
 app.set('view engine', 'ejs')
-app.set('views', path join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', function (req, res) {
-    res.send('Hello Margo')
+    res.render('index')
 })
 
 app.listen(3000)
