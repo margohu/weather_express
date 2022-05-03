2e769180990eccea08decc582f94fc819d2f0756
Margo Hutt <margo.hutt@ir.ee>
2022-05-03 12:03:59 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -26,4 +26,24 @@ app.get('/', function (req, res) {
             })
         })
 })
+
+app.post('/', function (req, res){
+    console.log(req.body)
+    res.redirect('/')
+    let city = req.body.cityname
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+        .then((responce) => {
+            return responce.json()
+        })
+        .then((data) => {
+            let description = data.weather[0].description
+            let city = data.name
+            let temp = Math.round(parseFloat(data.main.temp)-273.15)
+            res.render('index', {
+                description: description,
+                city: city,
+                temp: temp
+            })
+        })
+    })
 app.listen(3000)
