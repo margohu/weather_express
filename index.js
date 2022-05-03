062144524137542fb9654b669ae1c81ea1aa6211
Margo Hutt <margo.hutt@ir.ee>
2022-05-03 08:58:43 +0300

    Fisrt initial commit with ejs.

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.set('views', path join(__dirname, 'views'))
+
+app.get('/', function (req, res) {
+    res.send('Hello Margo')
+})
+
+app.listen(3000)
